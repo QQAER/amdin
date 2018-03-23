36f757813b7b4368969ed3d93c1d3739f9de8589
1025563641@qq.com <1025563641@qq.com>
2018-03-23 17:41:30 +0800

    2018-03-23 添加导入到xls表格功能

---
+++ data/login.php
@@ -0,0 +1,25 @@
+<?php
+/**接收客户端提交的用户名和密码，验证是否正确**/
+/**
+返回数据类型，使用JSON类型：
+正确： {"status": 1, "msg":"admin" }
+不存在： {"status": -404, "msg":"用户名或密码错误"}
+**/
+	header('Content-type:application/json');
+	$output=['status'=>0,'msg'=>''];
+	$uname = $_REQUEST['uaername'];
+	$upwd = $_REQUEST['password'];
+	$conn=mysqli_connect('127.0.0.1','root','','admin',3306);
+	mysqli_query($conn,'SET NAMES UTF8');
+	 $sql="SELECT * FROM admin_user WHERE name='$uname' AND pwd='$upwd'";
+	$retult=mysqli_query($conn,$sql);
+	$row = mysqli_fetch_assoc($retult);
+	if($row){
+		$output['status']=intval($row['pid']);//把字符串转化为整数
+		$output['msg']=$uname;
+	}else{
+		$output['status']=-404;
+		$output['msg']='用户名或密码错误';
+	}
+	echo json_encode($output)
+?>
